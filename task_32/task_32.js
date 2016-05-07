1c458504eb5df32405d7ea3d868de1f03fe0437d
GJWeigege <740187756@qq.com>
2016-05-07 09:01:42 +0800

    32

--- task_32/task_32.js
+++ task_32/task_32.js
@@ -1,14 +1,30 @@
 (function(){
-	var date = {
+	// function Data(data){
+	// 	this.label = data.label;
+	// 	this.type = data.type;
+	// 	this.rules = data.rules;
+	// 	this.success = data.success;
+	// 	this.fail = data.fail;
+	// }
+	// data.prototype.validator = function(first_argument) {
+	// 	// body...
+	// };
+	// data.prototype.init = function(first_argument) {
+	// 	// body...
+	// };
+	var data = {
         label: '名称',                    	// 表单标签
         type: 'input',                   	// 表单类型
-        validator: function () {			// 表单验证规
+        validator: function(){				// 表单验证规则
         	ds
+        },
+        init: function(){
+        	df
         },
         rules: '必填，长度为4-16个字符',    // 填写规则提示
         success: '格式正确',              	// 验证通过提示
         fail: '名称不能为空'               	// 验证失败提示
-	},dateArray = [];
+	},dataQueue=[];
 	function addHandler(element,type,handler){
 		if (window.addEventListener) {
 			element.addEventListener(type,handler,false);
@@ -20,6 +36,38 @@
 			element["on"+type] = handler;
 		}
 	};
+	function checkedType(id){
+		switch(id){
+			case "typeLabel1":{
+				data.label = "input";
+				data.type = "text";
+				break;
+			}
+			case "typeLabel2":{
+				data.label = "input";
+				data.type = "radio";
+				break;
+			}
+			case "typeLabel3":{
+				data.label = "input";
+				data.type = "check";
+				break;
+			}
+			case "typeLabel4":{
+				data.label = "select";
+				data.type = "select";
+				showOther(id);
+				break;
+			}
+			case "typeLabel5":{
+				data.label = "textarea";
+				data.type = "textfield";
+				break;
+			}
+		}
+		console.log(id);
+		console.log(data);
+	}
 	var doc = document,
 	myForm = doc.getElementById("myForm"),
 	checkType = doc.getElementById("checkType"),
@@ -28,21 +76,25 @@
 	addHandler(myForm,"click",function(event){
 		var e = event || window.event,
 		target = event.target || event.srcElement;
-		console.log(target.id);
+		// console.log(target.id);
 	});
 	addHandler(checkType,"click",function(event){
 		var e = event || window.event,
 		target = event.target || event.srcElement;
-		console.log(target.id);
+		if(target.tagName.toLowerCase() == "label"){
+			checkedType(target.id);
+		}
+		// console.log(target.id);
 	});
 	addHandler(addOther,"click",function(event){
 		var e = event || window.event,
 		target = event.target || event.srcElement;
 		console.log(target.id);
 	});
-	addHandler(add,"click",function(event){
-		var e = event || window.event,
-		target = event.target || event.srcElement;
-		console.log(target.id);
+	addHandler(add,"click",function(){
+		// var e = event || window.event,
+		// target = event.target || event.srcElement;
+		dataQueue.push(data);
+		// console.log(target.id);
 	});
 }());
